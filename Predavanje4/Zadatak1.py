6903b6d4f95de7ffdd30343c184508b6af8b51d0
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2023-10-19 19:07:29 +0200

    .

--- Predavanje4/Zadatak1.py
+++ Predavanje4/Zadatak1.py
@@ -1,1 +1,19 @@
 import linear_algebra_methods as li
+import numpy as np
+
+
+A = np.array([[2, 6, 0, 0, 0, 0, 0],
+            [1, 4, 5, 0, 0, 0, 0],
+            [0, 2, 6, 4, 0, 0, 0],
+            [0, 0, 3, 8, 3, 0, 0],
+            [0, 0, 0, 5, 6, 2, 0],
+            [0, 0, 0, 0, 5, 4, 1],
+            [0, 0, 0, 0, 0, 6, 2]])
+
+B = np.array([0, 1, 2, 3, 2, 1, 0])
+
+upp = np.array([6, 5, 4, 3, 2, 1])
+mid = np.array([2, 4, 6, 8, 6, 4, 2])
+low = np.array([1, 2, 3, 4, 5, 6])
+
+print(li.Thomas(low, mid, upp, B)*14)
--- Predavanje4/linear_algebra_methods.py
+++ Predavanje4/linear_algebra_methods.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 def Thomas(lower_diag, main_diag, upper_diag, solutions):
     '''Thomasova metoda za pronalazenje rjesenja linearnog sustava jednadzbi iz trodijagonalne matrice.'''
@@ -5,13 +7,15 @@ def Thomas(lower_diag, main_diag, upper_diag, solutions):
     b = main_diag
     c = upper_diag
     d = solutions
+    a.np.insert(0, 0)
+    c.append(0)
     n = len(d)
     c_trenutno = [0]*n
     d_trenutno = [0]*n
     rez = [0]*n
     c_trenutno[0] = c[0]/b[0]
     d_trenutno[0] = d[0]/b[0]
-    for i in range(1, n):
+    for i in range(1, n - 1):
         k = b[i] - a[i]*c_trenutno[i - 1]
         c_trenutno[i] = c[i]/k
         d_trenutno[i] = (d[i] - a[i]*d_trenutno[i - 1])/k
