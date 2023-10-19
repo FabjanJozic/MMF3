cc806dbb6bebf43311f33fffc2ba0d0c0d1660fc
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2023-10-19 19:23:24 +0200

    .

--- Predavanje4/linear_algebra_methods.py
+++ Predavanje4/linear_algebra_methods.py
@@ -7,8 +7,6 @@ def Thomas(lower_diag, main_diag, upper_diag, solutions):
     b = main_diag
     c = upper_diag
     d = solutions
-    a.np.insert(0, 0)
-    c.append(0)
     n = len(d)
     c_trenutno = [0]*n
     d_trenutno = [0]*n
@@ -16,9 +14,9 @@ def Thomas(lower_diag, main_diag, upper_diag, solutions):
     c_trenutno[0] = c[0]/b[0]
     d_trenutno[0] = d[0]/b[0]
     for i in range(1, n - 1):
-        k = b[i] - a[i]*c_trenutno[i - 1]
-        c_trenutno[i] = c[i]/k
-        d_trenutno[i] = (d[i] - a[i]*d_trenutno[i - 1])/k
+        c_trenutno[i] = c[i]/(b[i] - a[i]*c_trenutno[i])
+        d_trenutno[i] = (d[i] - a[i]*d_trenutno[i - 1]/(b[i] -a[i]*c_trenutno[i - 1]))
+    d_trenutno[n - 1] = (d[n - 1] - a[n - 1]*d_trenutno[n - 2])/(b[n - 1] - a[n - 1]*c_trenutno[n - 2])
     rez[n - 1] = d_trenutno[n - 1]
     for j in range(n -2, -1, -1):
         rez[j] = d_trenutno[j] - c_trenutno[j]*rez[j + 1]
