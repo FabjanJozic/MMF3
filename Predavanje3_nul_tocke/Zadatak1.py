fc110995c75c450cddcd5f659ed901903ad40a0d
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2023-11-21 16:08:28 +0100

    brisanje konstanti

--- Predavanje3_nul_tocke/Zadatak1.py
+++ Predavanje3_nul_tocke/Zadatak1.py
@@ -2,16 +2,24 @@ import nul_tocke as nt
 import numpy as np
 import matplotlib.pyplot as plt
 
-def y1(t, y1_0, A, B):
+
+y1_0 = 5
+A = 1
+B = 3
+y2_0 = 0.325
+C = 2
+D = 0.5
+
+def y1(t):
     return y1_0+A*np.cos(B*t)
 
-def y2(t, y2_0, C, D):
+def y2(t):
     return y2_0+C*np.exp(D*t)
 
-def fun(t, y1_0, A, B, y2_0, C, D):
-    return y1(t, y1_0, A, B) - y2(t, y2_0, C, D)
+def fun(t):
+    return y1(t) - y2(t)
 
-def deriv_fun(t, y1_0, A, B, y2_0, C, D):
+def deriv_fun(t):
     return -A*B*np.sin(B*t)-C*D*np.exp(D*t)
 
 
@@ -25,11 +33,11 @@ D = 0.5
 
 e = 1e-6 #epsilon
 
-print(nt.met_bisekcija(fun, 0.0, 2.8, e, y1_0, A, B, y2_0, C, D), nt.met_NR(fun, deriv_fun, 1.6, e, y1_0, A, B, y2_0, C, D),
-      nt.met_sekanta(fun, 1.6, e, y1_0, A, B, y2_0, C, D))
+print(nt.met_bisekcija(fun, 0.0, 2.8, e), nt.met_NR(fun, deriv_fun, 1.6, e),
+      nt.met_sekanta(fun, 1.6, e))
 
 x = [i for i in np.arange(0.0, 5.0, 0.01)]
-y = [fun(i, y1_0, A, B, y2_0, C, D) for i in x]
+y = [fun(i) for i in x]
 
 plt.plot(x, y)
 plt.grid(True)
--- Predavanje3_nul_tocke/nul_tocke.py
+++ Predavanje3_nul_tocke/nul_tocke.py
@@ -3,16 +3,16 @@
 #Vrijednosti A, B, C, D, E i F su konstante. 
 
 
-def met_bisekcija(function, a, b, epsilon, A, B, C, D, E, F):
+def met_bisekcija(function, a, b, epsilon):
     '''Metoda bisekcije ili raspolavljanja intervala [a,b] za trazenje nul-tocke funkcije u intervalu [a,b], uz ogranicenje epsilon.'''
     c =(a+b)/2
     k = 1
     while abs(c) > epsilon and k <= 500:
-        if function(a, A, B, C, D, E, F)*function(c, A, B, C, D, E, F) < 0:
+        if function(a)*function(c) < 0:
             a = a
             b = c
             c = (a+b)/2
-        if function(a, A, B, C, D, E, F)*function(c, A, B, C, D, E, F) > 0:
+        if function(a)*function(c) > 0:
             a = c
             b = b
             c = (a+b)/2
@@ -21,20 +21,20 @@ def met_bisekcija(function, a, b, epsilon, A, B, C, D, E, F):
         k +=1
     return c, k
 
-def met_NR(function, deriv_function, x, epsilon, A, B, C, D, E, F):
+def met_NR(function, deriv_function, x, epsilon):
     '''Newton-Raphsonova metoda za trazenje nul-tocke funkcije u okolini tocke x uz poznavanje derivacije funkcije, uz ogranicenje epsilon.'''
     k = 1
-    while abs(function(x, A, B ,C, D, E, F)/deriv_function(x, A, B, C, D, E, F)) > epsilon and k <= 500:
-        x -= function(x, A, B, C, D, E, F)/deriv_function(x, A, B, C, D, E, F)
+    while abs(function(x)/deriv_function(x)) > epsilon and k <= 500:
+        x -= function(x)/deriv_function(x)
         k += 1
     return x, k
     
-def met_sekanta(function, x, epsilon, A, B, C, D, E, F):
+def met_sekanta(function, x, epsilon):
     '''Metode sekante za trazenje nul-tocke funkcije u okolini tocke x uz nepoznatu derivaciju funkcije, uz ogranicenje epsilon.'''
-    def deriv_function(function, x, epsilon, A, B, C, D, E, F):
-        return (function(x+epsilon, A , B, C, D, E, F) - function(x-epsilon, A, B, C, D, E, F))/(2*epsilon)
+    def deriv_function(function, x, epsilon):
+        return (function(x+epsilon) - function(x-epsilon))/(2*epsilon)
     k = 1
-    while abs(function(x, A, B, C, D, E, F)/deriv_function(function, x, epsilon, A, B, C, D, E, F)) > epsilon and k <= 500:
-        x -= function(x, A, B, C, D, E, F)/deriv_function(function, x, epsilon, A, B, C, D, E, F)
+    while abs(function(x)/deriv_function(function, x, epsilon)) > epsilon and k <= 500:
+        x -= function(x)/deriv_function(function, x, epsilon)
         k +=1
     return x, k
