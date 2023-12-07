48b2d4bc004e0755160d9ed30fc36e651e6b608c
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2023-12-07 20:25:44 +0100

    zufu

--- Predavanje10_ODJ_Euler/Zadatak1.py
+++ Predavanje10_ODJ_Euler/Zadatak1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import diferencijalne_jednadzbe as dj
 
 m = 0.2 #masa
-l = 0.02484902028828339 #duljina njihala
+l = 0.2484902028828339 #duljina njihala
 y0 = 4 #pocetni kut
 g = 9.81 #gravitacijska akceleracija
 v0 = 0.0 #pocetna brzina
@@ -17,7 +17,7 @@ def analiticko(t, x, v):
 def numericko(t, x, v):
     return -g/l*np.sin(x)
 
-T = 2*np.pi*np.sqrt(l/g)
+T = 2*np.pi*np.sqrt(l/g) #period
 t1 = 1*T
 t2 = 20*T
 
@@ -34,26 +34,57 @@ axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 500, t2)[0], dj.Euler(0.0 , y0_r, 
           color='blue', lw=1.1, label='$y_{E,500}(t)$')
 axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 1000, t2)[0], dj.Euler(0.0 , y0_r, v0, numericko, 1000, t2)[1],
           color='green', lw=1.1, label='$y_{E,1000}(t)$')
-axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 5000, t2)[0], dj.Euler(0.0 , y0_r, v0, numericko, 5000, t2)[1],
-          color='orange', lw=1.1, label='$y_{E,5000}(t)$')
+axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 20000, t2)[0], dj.Euler(0.0 , y0_r, v0, numericko, 20000, t2)[1],
+          color='orange', lw=1.1, label='$y_{E,20000}(t)$')
 axes.plot(t, theta, color='purple', lw=1.1, label='$y_{a}(t)$')
 axes.grid(lw=0.5)
 axes.set_xlim(t1, t2)
 axes.legend(loc='best')
 plt.show()'''
 
-'''fig = plt.figure(figsize=(11,5), dpi=110)
+fig = plt.figure(figsize=(11,5), dpi=110)
 axes = fig.add_axes([0.15, 0.15, 0.75, 0.70])
 plt.rcParams.update({'font.size': 9})           #type: ignore
-axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 10000, t2)[0], dj.Euler(0.0 , y0_r, v0, numericko, 10000, t2)[1],
+'''axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 100000, t2)[0], dj.Euler(0.0 , y0_r, v0, numericko, 100000, t2)[1],
           color='blue', lw=1.1, label='Euler $N = 10000$')
-axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 1000, t2)[0], dj.Euler(0.0 , y0_r, v0, numericko, 1000, t2)[1],
-          color='purple', lw=1.1, label='Euler $N/10 = 1000$')
+axes.plot(dj.Euler(0.0 , y0_r, v0, numericko, 10000, t2)[0], dj.Euler(0.0 , y0_r, v0, numericko, 10000, t2)[1],
+          color='purple', lw=1.1, label='Euler $N/10 = 1000$')'''
 axes.plot(dj.RK4(0.0 , y0_r, v0, numericko, 10000, t2)[0], dj.RK4(0.0 , y0_r, v0, numericko, 10000, t2)[1],
           color='orange', lw=1.1, label='RK4 $N = 10000$')
 axes.plot(dj.RK4(0.0 , y0_r, v0, numericko, 1000, t2)[0], dj.RK4(0.0 , y0_r, v0, numericko, 1000, t2)[1],
           color='red', lw=1.1, label='RK4 $N/10 = 1000$')
 axes.grid(lw=0.5)
 #axes.set_xlim(t1, t2)
+axes.legend(loc='best')
+plt.show()
+
+'''t = []
+theta_4 = []
+theta_8 = []
+theta_16 = []
+theta_32 = []
+for i in np.arange(0.0, 7*T+1e-5, 1e-5):
+    t.append(i)
+    theta_4.append(y0_r*np.cos(i*np.sqrt(g/l)))
+    theta_8.append(y0_r*2*np.cos(i*np.sqrt(g/l)))
+    theta_16.append(y0_r*4*np.cos(i*np.sqrt(g/l)))
+    theta_32.append(y0_r*8*np.cos(i*np.sqrt(g/l)))
+
+fig = plt.figure(figsize=(11,5), dpi=110)
+axes = fig.add_axes([0.15, 0.15, 0.75, 0.70])
+plt.rcParams.update({'font.size': 9})           #type: ignore
+axes.plot(dj.RK4(0.0 , y0_r, v0, numericko, 1000, 7*T)[0], dj.RK4(0.0 , y0_r, v0, numericko, 1000, 7*T)[1],
+          color='green', lw=1.1, label='RK4, $y_{0} = 4^{o}$', linestyle='--')
+axes.plot(dj.RK4(0.0 , y0_r*2, v0, numericko, 1000, 7*T)[0], dj.RK4(0.0 , y0_r*2, v0, numericko, 1000, 7*T)[1],
+          color='red', lw=1.1, label='RK4, $y_{0} = 8^{o}$', linestyle='--')
+axes.plot(dj.RK4(0.0 , y0_r*4, v0, numericko, 1000, 7*T)[0], dj.RK4(0.0 , y0_r*4, v0, numericko, 1000, 7*T)[1],
+          color='yellow', lw=1.1, label='RK4, $y_{0} = 16^{o}$', linestyle='--')
+axes.plot(dj.RK4(0.0 , y0_r*8, v0, numericko, 1000, 7*T)[0], dj.RK4(0.0 , y0_r*8, v0, numericko, 1000, 7*T)[1],
+          color='blue', lw=1.1, label='RK4, $y_{0} = 32^{o}$', linestyle='--')
+axes.plot(t, theta_4, color='green', lw=1.2, label='$y_{a}, y_{0} = 4^{o}$')
+axes.plot(t, theta_8, color='red', lw=1.2, label='$y_{a}, y_{0} = 8^{o}$')
+axes.plot(t, theta_16, color='yellow', lw=1.2, label='$y_{a}, y_{0} = 16^{o}$')
+axes.plot(t, theta_32, color='blue', lw=1.2, label='$y_{a}, y_{0} = 32^{o}$')
+axes.grid(lw=0.5)
 axes.legend(loc='best')
 plt.show()'''
--- Predavanje10_ODJ_Euler/diferencijalne_jednadzbe.py
+++ Predavanje10_ODJ_Euler/diferencijalne_jednadzbe.py
@@ -11,13 +11,11 @@ def Euler(t0, x0, v0, function, N, t):
     X = [x0] #pocetni polozaj
     V = [v0] #pocetna brzina
     A = [function(t0, x0, v0)] #pocetna akceleracija
-    i = 1
     while T[-1] <= t:
-        T.append(i*h)
-        V.append(V[-1]+A[-1]*h)
+        T.append(T[-1]+h)
         X.append(X[-1]+V[-1]*h)
+        V.append(V[-1]+A[-1]*h)
         A.append(function(T[-1], X[-1], V[-1]))
-        i += 1
     return T, X, V, A
 
 def pred_kor(t0, x0, v0, function, N, t):
@@ -32,7 +30,6 @@ def pred_kor(t0, x0, v0, function, N, t):
     X = [x0] #pocetni polozaj
     V = [v0] #pocetna brzina
     A = [function(t0, x0, v0)] #pocetna akceleracija
-    i = 1
     while T[-1] <= t:
         t = T[-1]
         x = X[-1]
@@ -41,11 +38,10 @@ def pred_kor(t0, x0, v0, function, N, t):
         k1x = v
         k2v = function(t+h, x+h*k1x, v+h*k1v)
         k2x = v+h*k1v
-        T.append(i*h)
+        T.append(T[-1]+h)
         X.append(x+(k1x+k2x)*h/2)
         V.append(v+(k1v+k2v)*h/2)
         A.append(function(T[-1], X[-1], V[-1]))
-        i += 1
     return T, X, V, A
 
 def RK4(t0, x0, v0, function, N, t):
@@ -60,7 +56,6 @@ def RK4(t0, x0, v0, function, N, t):
     X = [x0] #pocetni polozaj
     V = [v0] #pocetna brzina
     A = [function(t0, x0, v0)] #pocetna akceleracija
-    i = 1
     while T[-1] <= t:
         t = T[-1]
         x = X[-1]
@@ -73,9 +68,8 @@ def RK4(t0, x0, v0, function, N, t):
         k3x = v+h/2*k2v
         k4v = function(t+h, x+h*k3x, v+h*k3v)
         k4x = v+h*k3v
-        T.append(i*h)
-        X.append(x+(k1x+k2x+k3x+k4x)*h/6)
-        V.append(v+(k1v+k2v+k3v+k4v)*h/6)
+        T.append(t+h)
+        X.append(x+(k1x+2*k2x+2*k3x+k4x)*h/6)
+        V.append(v+(k1v+2*k2v+2*k3v+k4v)*h/6)
         A.append(function(T[-1], X[-1], V[-1]))
-        i += 1
     return T, X, V, A
