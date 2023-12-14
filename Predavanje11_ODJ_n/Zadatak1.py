ec0b2e369bcb82e1350428b4d7cb5e4bcb5d335f
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2023-12-14 18:36:54 +0100

    derivacija n-tog reda

---
+++ Predavanje11_ODJ_n/Zadatak1.py
@@ -0,0 +1,25 @@
+import diferencijalne_jednadzbe as dj
+import numpy as np
+import matplotlib.pyplot as plt 
+
+X0 = [0.0, 0.9, 29.0] #pocetni uvjeti
+m = 4.7 #masa
+F = 5.2 #sila
+k = 400
+def idi(Y): #funkcija alceleracije
+    return F/m
+N = 6000
+tN = 3.2 #krajnje vrijeme gibanja
+
+t, x, v, a = dj.Euler_n(X0, idi, N, tN)[0], dj.Euler_n(X0, idi, N, tN)[1], dj.Euler_n(X0, idi, N, tN)[2], dj.Euler_n(X0, idi, N, tN)[3]
+
+fig = plt.figure(figsize=(11,5), dpi=110)
+axes = fig.add_axes([0.15, 0.15, 0.75, 0.70])
+plt.rcParams.update({'font.size': 9})           #type: ignore
+axes.plot(t, x, color='blue', label='put')
+axes.plot(t, v, color='green', label='brzina')
+axes.plot(t, a, color='red', label='akceleracija')
+axes.grid(lw=0.5)
+axes.legend(loc='best')
+plt.show()
+
