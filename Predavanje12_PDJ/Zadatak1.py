ea8e90b204cc4f3ec0dc506a6791747342ba80d9
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2024-01-06 16:54:26 +0100

    USPIA

--- Predavanje12_PDJ/Zadatak1.py
+++ Predavanje12_PDJ/Zadatak1.py
@@ -5,7 +5,6 @@ import sys
 
 sys.getdefaultencoding()
 
-
 def rho(x):
     if x >= 2.0 and x <= 5.0:
         return 5.5
@@ -15,34 +14,35 @@ def rho(x):
 j = [0, 100, 200, 300, 400]
 t = [0.5*J for J in j]
 
-P1 = [0.0, 20.0, 0.0, t[0]] #pocetni uvjeti
-P2 = [0.0, 20.0, 0.0, t[1]]
-P3 = [0.0, 20.0, 0.0, t[2]]
-P4 = [0.0, 20.0, 0.0, t[3]]
-P5 = [0.0, 20.0, 0.0, t[4]]
+dx, dt = 0.2, 0.5
 
-N = 100
+P1 = [0.0, 20.0, 0.0, t[0], dx, dt] #pocetni uvjeti
+P2 = [0.0, 20.0, 0.0, t[1], dx, dt]
+P3 = [0.0, 20.0, 0.0, t[2], dx, dt]
+P4 = [0.0, 20.0, 0.0, t[3], dx, dt]
+P5 = [0.0, 20.0, 0.0, t[4], dx, dt]
 
-D1 = di.D_exp(rho, P1, N, j[0]) #vrijednosti funkcije difuzije
-D2 = di.D_exp(rho, P2, N, j[1])
-D3 = di.D_exp(rho, P3, N, j[2])
-D4 = di.D_exp(rho, P4, N, j[3])
-D5 = di.D_exp(rho, P5, N, j[4])
+Dif = 1e-2 #difuzijska konstanta
 
-X = [x/(20.0/N) for x in np.arange(0.0, 20.0 + 20.0/N, 20.0/N)]
+D1 = di.D_exp(rho, P1, Dif) #vrijednosti funkcije difuzije
+D2 = di.D_exp(rho, P2, Dif)
+D3 = di.D_exp(rho, P3, Dif)
+D4 = di.D_exp(rho, P4, Dif)
+D5 = di.D_exp(rho, P5, Dif)
 
-fig = plt.figure(figsize=(9,6), dpi=120)
-axes = fig.add_axes([0.15, 0.15, 0.75, 0.70])
-plt.rcParams.update({'font.size': 8}) #type: ignore
-axes.plot(X, D1, label='t = {}$\u0394$x'.format(j[0]), lw=0.8, color='lightblue')
-#axes.plot(X, D2, label='t = {}$\u0394$x'.format(j[1]), lw=0.8, color='blue')
-#axes.plot(X, D3, label='t = {}$\u0394$x'.format(j[2]), lw=0.8, color='cyan')
-#axes.plot(X, D4, label='t = {}$\u0394$x'.format(j[3]), lw=0.8, color='green')
-#axes.plot(X, D5, label='t = {}$\u0394$x'.format(j[4]), lw=0.8, color='orange')
+X = [x/dx for x in np.arange(0.0, 20.0+dx, dx)]
+
+fig = plt.figure(figsize=(7,5), dpi=120)
+axes = fig.add_axes([0.10, 0.10, 0.85, 0.85])
+plt.rcParams.update({'font.size': 8}) #type:ignore
+axes.plot(X, D1, label='t = {}$\u0394$t'.format(j[0]), lw=1.4, color='purple')
+axes.plot(X, D2, label='t = {}$\u0394$t'.format(j[1]), lw=1.4, color='blue')
+axes.plot(X, D3, label='t = {}$\u0394$t'.format(j[2]), lw=1.4, color='cyan')
+axes.plot(X, D4, label='t = {}$\u0394$t'.format(j[3]), lw=1.4, color='green')
+axes.plot(X, D5, label='t = {}$\u0394$t'.format(j[4]), lw=1.4, color='orange')
 axes.grid(lw=0.5)
 axes.set_xlabel('x / $\u0394$x')
 axes.set_ylabel('$\u03C1(x,t)$ / kgm$^{-1}$')
 axes.legend(loc='best')
-axes.set_title('Fazni dijagram matematičkog njihala')
+axes.set_title('Difuzija')
 plt.show()
-
--- Predavanje12_PDJ/difuzija.py
+++ Predavanje12_PDJ/difuzija.py
@@ -1,25 +1,26 @@
 import numpy as np
 
-def D_exp(g_x, xt0_N, N, M):
-    '''Eksplicitna metoda rjesavanje difuzijske parcijalne diferencijalne jednadzbe.
+def D_exp(g_x, xt0_N, D):
+    '''Eksplicitna metoda rjesavanje difuzijske parcijalne
+    diferencijalne jednadzbe.
     \ng_x ------ funkcija pocetnih uvjeta za x varijablu
-    \nxt0_N ---- pocetni i krajnji uvjeti polozaja pa vremena
-    \nN -------- broj x koraka
-    \nM -------- broj t koraka'''
-    dx = (xt0_N[1]-xt0_N[0])/(N+1) #korak polozaja
-    dt = (xt0_N[3]-xt0_N[2])/(M+1) #korak vremena
-    alpha = dt/(dx**2)
-    '''Vrijednost funkcije difuzije dobiva se rekurzivno preko vrijednosti varijable polozaja.'''
-    dif_p = []*(N+1)
-    dif_r = [0]*(N+1)
-    for u in range(N+1):
-        dif_p.append(g_x(u*dx+xt0_N[0]))
-    for j in range(1, M+1): #vrijeme
-        if j == 100:
-            m = dif_r
+    \nxt0_N ---- vektor pocetnih uvjeta
+    \nD -------- konstanta difuzije'''
+    dx = xt0_N[4] #korak polozaja
+    dt = xt0_N[5] #korak vremena
+    N = int(xt0_N[1]/dx) #broj tocaka u prostoru
+    M = int(xt0_N[3]) #broj tocaka u vremenu
+    alpha = D*dt/(dx**2)
+    '''Vrijednost funkcije difuzije dobiva se rekurzivno preko
+    vrijednosti varijable polozaja.'''
+    dif_p = np.zeros(N+1)
+    dif_r = np.zeros(N+1)
+    for u in range(len(dif_p)):
+        dif_p[u] = g_x(xt0_N[0]+u*dx)
+    for j in range(M+1): #vrijeme
         for i in range(1, N): #polozaj
             dif_r[i] = alpha*dif_p[i+1]+(1-2*alpha)*dif_p[i]+alpha*dif_p[i-1]
-        dif_p = dif_r
+        dif_p = np.copy(dif_r)
     return dif_r
 
             
